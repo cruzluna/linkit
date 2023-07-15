f2e2cefa9e6051c4ef7ce2a5322fe1566f2849ce
Cruz <lunac8@unlv.nevada.edu>
2023-07-15 12:56:22 -0400

    update script logic

--- exec.py
+++ exec.py
@@ -5,18 +5,21 @@ If permission denied error, run the following command:
     chmod +x exec.py
 """
 import os
+import subprocess
 
 beginning_message = "Running check before push"
 bm = os.system("echo %s" %beginning_message)
-build_command = os.system("npm run build")
 
-print(f"Build command exited with code: {build_command}")
-if build_command == 0: 
+build_process = subprocess.Popen("npm run build", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+stdout, stderr = build_process.communicate()
+
+print(stdout.decode())
+if build_process.returncode == 0:
     print("Build succeeded, time to push")
+    current_working_directory = os.getcwd()
+    print("*---------------------*")
+    print("Push project --> ", current_working_directory.split('/')[-1])
+    print("*---------------------*")
 else:
+    print(stderr.decode())
     print("Build failed, review npm run build error log")
-
-current_working_directory = os.getcwd()
-print("*---------------------*")
-print("Push project --> ", current_working_directory.split('/')[-1])
-print("*---------------------*")
