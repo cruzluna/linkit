b4669ad99a559879b6535b14f5a12f4c0974d26b
Cruz <lunac8@unlv.nevada.edu>
2023-08-23 11:37:17 -0700

    bug: fix error modal handleOpen, no args needed

--- src/components/errorModal.tsx
+++ src/components/errorModal.tsx
@@ -16,13 +16,13 @@ export function ErrorModal({ open, onOpen }: errorModalProps) {
 
   return (
     <>
-      <Dialog open={open} handler={() => onOpen(false)}>
+      <Dialog open={open} handler={onOpen}>
         <DialogHeader>Error submitting form.</DialogHeader>
         <DialogBody divider>
           If problem persists, email notespaceai@gmail.com{" "}
         </DialogBody>
         <DialogFooter>
-          <Button variant="gradient" color="red" onClick={() => onOpen(false)}>
+          <Button variant="gradient" color="red" onClick={onOpen}>
             <span>Try again</span>
           </Button>
         </DialogFooter>
