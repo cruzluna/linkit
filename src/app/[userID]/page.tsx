1de988534523377a4705f54299920270fb338e39
Cruz <lunac8@unlv.nevada.edu>
2023-08-22 19:27:00 -0700

    WIP: added links iteratively to userID page

--- src/app/[userID]/page.tsx
+++ src/app/[userID]/page.tsx
@@ -104,75 +104,36 @@ const page = async ({ params }: PageProps) => {
           </figure>
         </div>
 
-        <div className="flex flex-col-1">
-          <div className="max-w-xs mx-auto py-2">
-            <div className="flex flex-col-1 items-start space-y-4  ">
-              <div className="flex items-start w-80 text-center rounded-lg border border-gray-400 bg-[#FAFAFA] px-5 py-4 text-lg leading-6 font-medium shadow-md hover:shadow-xl transition ease-in-out duration-150">
-                <p className="px-3 h-6 w-6"> 🔗 </p>
-                <p className="text-[#1B1B1B] mx-auto truncate">Loading...</p>
-                <div className="px-auto mt-0.5">
-                  <svg
-                    className="h-5 w-4 text-[#1B1B1B]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-                    />
-                  </svg>
+        {user.links.map((link) => {
+          return (
+            <div className="flex flex-col-1">
+              <div className="max-w-xs mx-auto py-2">
+                <div className="flex flex-col-1 items-start space-y-4  ">
+                  <div className="flex items-start w-80 text-center rounded-lg border border-gray-400 bg-[#FAFAFA] px-5 py-4 text-lg leading-6 font-medium shadow-md hover:shadow-xl transition ease-in-out duration-150">
+                    <p className="px-1 mr-3 h-6 w-6"> 🔗 </p>
+                    {/* TODO: make this a clickable link*/}
+                    <p className="text-[#1B1B1B] mx-auto truncate">
+                      {link.url}
+                    </p>
+                    <div className="px-auto mt-0.5">
+                      <svg
+                        className="h-5 w-4 text-[#1B1B1B]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="flex flex-col-1">
-          <div className="max-w-xs mx-auto py-2">
-            <div className="flex flex-col-1 items-start space-y-4  ">
-              <div className="flex items-start w-80 text-center rounded-lg border border-gray-400 bg-[#FAFAFA] px-5 py-4 text-lg leading-6 font-medium shadow-md hover:shadow-xl transition ease-in-out duration-150">
-                <p className="px-3 h-6 w-6"> 🔗 </p>
-                <p className="text-[#1B1B1B] mx-auto truncate">Loading...</p>
-                <div className="px-auto mt-0.5">
-                  <svg
-                    className="h-5 w-4 text-[#1B1B1B]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col-1">
-          <div className="max-w-xs mx-auto py-2">
-            <div className="flex flex-col-1 items-start space-y-4  ">
-              <div className="flex items-start w-80 text-center rounded-lg border border-gray-400 bg-[#FAFAFA] px-5 py-4 text-lg leading-6 font-medium shadow-md hover:shadow-xl transition ease-in-out duration-150">
-                <p className="px-3 h-6 w-6"> 🔗 </p>
-                <p className="text-[#1B1B1B] mx-auto truncate">Loading...</p>
-                <div className="px-auto mt-0.5">
-                  <svg
-                    className="h-5 w-4 text-[#1B1B1B]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
