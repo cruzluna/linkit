9d4bb7b7709d247ddb9d08485f9d1c7f5507a5f7
Cruz <lunac8@unlv.nevada.edu>
2023-08-31 18:28:45 -0700

    bug: fix login redirect to /user/links

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 export default function Login() {
   return (
     <div className="flex items-center justify-center">
-      <SignIn path={"/login"} redirectUrl={"/links"} />
+      <SignIn path={"/login"} redirectUrl={"/user/links"} />
     </div>
   );
 }
