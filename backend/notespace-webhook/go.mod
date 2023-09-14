74041077f3ce9507694c3df9e1d0b5f35f9aaf25
Cruz <lunac8@unlv.nevada.edu>
2023-09-13 21:03:32 -0700

    convert to monorepo

---
+++ backend/notespace-webhook/go.mod
@@ -0,0 +1,20 @@
+module notespace/webhook
+
+go 1.19
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/svix/svix-webhooks v1.12.0
+)
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365 // indirect
+	github.com/steebchen/prisma-client-go v0.22.1 // indirect
+	github.com/takuoki/gocase v1.0.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ backend/notespace-webhook/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	svix "github.com/svix/svix-webhooks/go"
+
+	"github.com/joho/godotenv"
+)
+
+type ClerkHookStruct struct {
+	Data struct {
+		CreatedAt int64  `json:"created_at"`
+		ID        string `json:"id"`
+		UpdatedAt int64  `json:"updated_at"`
+		Username  string `json:"username"`
+	} `json:"data"`
+	Object string `json:"object"`
+	Type   string `json:"type"`
+}
+
+func main() {
+	fmt.Printf("\x1b[%dm%s\x1b[0m", 34, "SERVER STARTED\n")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// var clerkEndpoint string = os.Getenv("CLERK_TEST_ENDPOINT")
+
+	secret := os.Getenv("SIGN_SECRET_TEST")
+
+	wh, err := svix.NewWebhook(secret)
+	if err != nil {
+		fmt.Println("ERROR WH")
+		log.Fatal(err)
+	}
+
+	// handle endpoint
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("MADE IT IN here 2")
+		headers := r.Header
+
+		payload, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Print("ERROR 1", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = wh.Verify(payload, headers)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Do something with the message...
+
+		// CREATE A struct with eventtype, clerkID, username
+		var dat ClerkHookStruct
+
+		if err := json.Unmarshal(payload, &dat); err != nil {
+			panic(err)
+		}
+		data, _ := json.Marshal(dat)
+		fmt.Println(string(data))
+		fmt.Println("DATA: ", dat)
+
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	httpErr := http.ListenAndServe(":3000", nil)
+	fmt.Println("ERROR: ", httpErr)
+
+	// 1. Listen for webhook post
+
+	// 2. Upsert user username , leave create field empty
+	// user clerkID to write to DB
+}
