1c0eb9905fcf84072aeac5d66d8a8d7037d6fa42
Cruz <lunac8@unlv.nevada.edu>
2024-07-19 22:58:57 -0400

    fix get started button action

--- frontend/linkit/src/components/faqs.tsx
+++ frontend/linkit/src/components/faqs.tsx
@@ -1,20 +1,25 @@
-
-import 'tailwindcss/tailwind.css'
-import {
-  Button,
-} from "@material-tailwind/react";
-import Image from 'next/image';
-import product from "../assets/product.svg"
+import "tailwindcss/tailwind.css";
+import { Button } from "@material-tailwind/react";
+import Image from "next/image";
+import product from "../assets/product.svg";
+import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 
 export default function FAQs(): JSX.Element {
+  const router: AppRouterInstance = useRouter();
   return (
-    <section className="bg-[#12141F] flex justify-center py-16 text-[#FAFAFA]" id='faqs'>
-   
+    <section
+      className="bg-[#12141F] flex justify-center py-16 text-[#FAFAFA]"
+      id="faqs"
+    >
       <div className="max-w-lg">
         <div className="space-y-4 ml-4 mr-4">
           <div className="mx-auto max-w-lg text-center">
-            <div className="lg:text-5xl text-3xl font-bold mb-10"><span className='bg-gradient-to-r from-teal-300 via-blue-500 to-noto-purple bg-clip-text text-transparent'>FAQs</span></div>
-          
+            <div className="lg:text-5xl text-3xl font-bold mb-10">
+              <span className="bg-gradient-to-r from-teal-300 via-blue-500 to-noto-purple bg-clip-text text-transparent">
+                FAQs
+              </span>
+            </div>
           </div>
           <details className="group [&_summary::-webkit-details-marker]:hidden">
             <summary className="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-[#1C202F] p-4">
@@ -28,12 +33,15 @@ export default function FAQs(): JSX.Element {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
               </svg>
             </summary>
-            <p className="mt-4 px-4 leading-relaxed">
-              No, notespace is free!
-            </p>
+            <p className="mt-4 px-4 leading-relaxed">No, notespace is free!</p>
           </details>
 
           <details className="group [&_summary::-webkit-details-marker]:hidden">
@@ -48,7 +56,12 @@ export default function FAQs(): JSX.Element {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
               </svg>
             </summary>
             <p className="mt-4 px-4 leading-relaxedbg-[#1C202F]">
@@ -57,15 +70,16 @@ export default function FAQs(): JSX.Element {
           </details>
           <div className="flex flex-1 flex-col items-center lg:items-center ">
             <h2 className="text-3xl md:text-4 lg:text-5xl text-center mt-10 mb-10 font-extrabold md:font-bold">
-              Ready to <span className='bg-gradient-to-r from-teal-300 via-blue-500 to-noto-purple bg-clip-text text-transparent'>Start?</span>
+              Ready to{" "}
+              <span className="bg-gradient-to-r from-teal-300 via-blue-500 to-noto-purple bg-clip-text text-transparent">
+                Start?
+              </span>
             </h2>
             <div className="flex justify-center flex-wrap py-px">
               <button
                 type="button"
                 className="bg-noto-purple block border border-noto-purple hover:bg-[#12141F] text-white font-bold px-9 py-3 rounded"
-                onClick={() =>
-                  (window.location.href = "https://eepurl.com/ilQO8-")
-                }
+                onClick={() => router.push("/signup")}
               >
                 Get Started
               </button>
@@ -74,6 +88,5 @@ export default function FAQs(): JSX.Element {
         </div>
       </div>
     </section>
-
   );
 }
